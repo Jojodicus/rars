4e1e74b92089cd9225ed8b333a32c84d6348ceaa
jojodicus <jojodicus@gmail.com>
2023-06-01 01:24:35 +0200

    color and error handling

--- src/rars/tools/PipelineVisualizer.java
+++ src/rars/tools/PipelineVisualizer.java
@@ -55,7 +55,7 @@ import java.util.Observable;
 // TODO: javadoc
 
 public class PipelineVisualizer extends AbstractToolAndApplication {
-
+    // TODO: help button
     // TODO: standalone application
     // TODO: other pipeline types
     // TODO: backstep
@@ -79,6 +79,7 @@ public class PipelineVisualizer extends AbstractToolAndApplication {
     private static final String VERSION = "1.0 (Johannes Dittrich)";
     private static final String HEADING = "Visualizer for advanced pipelining on RARS";
 
+    private JPanel panel;
     private JTable pipeline;
     private DefaultTableModel model;
     private int lastAddress = -1; // TODO: is this really needed?
@@ -108,7 +109,7 @@ public class PipelineVisualizer extends AbstractToolAndApplication {
             colors.add(new HashMap<>());
         }
 
-        JPanel panel = new JPanel(new BorderLayout());
+        panel = new JPanel(new BorderLayout());
 
         model = new DefaultTableModel();
         model.addColumn("IF");
@@ -214,6 +215,7 @@ public class PipelineVisualizer extends AbstractToolAndApplication {
             }
 
             if (ret != null) {
+                failsafe = 0;
                 System.err.println("unexpected return value: " + statementToString(stmt));
                 System.err.println("expected return value: " + statementToString(ret));
                 System.err.println("pipeline: [" + statementToString(currentPipeline[0]) + ", " + statementToString(currentPipeline[1]) + ", " + statementToString(currentPipeline[2]) + ", " + statementToString(currentPipeline[3]) + ", " + statementToString(currentPipeline[4]) + "]");
@@ -224,7 +226,10 @@ public class PipelineVisualizer extends AbstractToolAndApplication {
         }
 
         if (failsafe == 0) {
-            // TODO: infobox
+            JOptionPane.showMessageDialog(panel, "VAPOR: could not predict pipeline", "VAPOR", JOptionPane.ERROR_MESSAGE);
+            reset();
+            connectButton.doClick();
+            // TODO: telemetry
             System.err.println("VAPOR: could not predict pipeline");
         }
     }
@@ -243,8 +248,9 @@ public class PipelineVisualizer extends AbstractToolAndApplication {
         // control hazard resolved // TODO: optimize check
         ProgramStatement wb = currentPipeline[STAGE.WB];
         if (wb != null && wb.getInstruction() instanceof Branch) {
-            // update last prediction TODO: add coloring for uncertianty
+            // update last prediction
             model.setValueAt(statementToString(next), model.getRowCount()-1, STAGE.IF);
+            colors.get(STAGE.IF).remove(model.getRowCount()-1);
             currentPipeline[STAGE.IF] = next;
 
             // nextInMem = next;
@@ -348,10 +354,17 @@ public class PipelineVisualizer extends AbstractToolAndApplication {
         });
 
         // add color
+
+        // control hazard
         int row = model.getRowCount()-1;
         int col = hasControlHazardInt();
         if (col != -1) {
             colors.get(col).put(row, Color.YELLOW);
+        }
+
+        // still uncertain fetch (from control hazard - limitation of simulation)
+        if (currentPipeline[STAGE.WB] != null && currentPipeline[STAGE.WB].getInstruction() instanceof Branch) {
+            colors.get(STAGE.IF).put(row, Color.LIGHT_GRAY);
         }
 
         // scroll to bottom
