15d315f2638c67f3fda5d4ffeb684d4cf9fb983d
jojodicus <jojodicus@gmail.com>
2023-06-13 13:41:59 +0200

    revert changing example

--- examples/mastermind.s
+++ examples/mastermind.s
@@ -60,7 +60,7 @@ secretString: .string "solu\n"
 
 _start:
 # First generate secret code and store it in SecretCode buffer
-        call generateCode
+        jal ra, generateCode
         
 #read user number and check for correctness
 user:
